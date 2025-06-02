609d97c51731e01d07b11aed5a5d07ef1b419e82
Sunil.s <sunil.sn@i-exceed.com>
2025-06-02 20:20:55 +0530

    Account Deatisl

--- Frontend/Applications/MicroApplications/account-microapp/src/Screens/AccountsPage.js
+++ Frontend/Applications/MicroApplications/account-microapp/src/Screens/AccountsPage.js
@@ -36,78 +36,166 @@ const AccountsPage = () => {
   function handleDelete(row) {
     console.log("Delete clicked", row);
   }
-  const data = [
+  const AllAccounts = [
+    { Acc: "12313123", Type: "Savings", Bal: "123.78", Branch: "Bangalore" },
+    { Acc: "12313123", Type: "Current", Bal: "123.78", Branch: "Bangalore" },
+    { Acc: "12313123", Type: "Deposits", Bal: "123.78", Branch: "Bangalore" },
+    { Acc: "12313123", Type: "Loans", Bal: "123.78", Branch: "Bangalore" },
+    { Acc: "12313123", Type: "PPF", Bal: "123.78", Branch: "Bangalore" },
+    { Acc: "12313123", Type: "OD", Bal: "123.78", Branch: "Bangalore" },
+    { Acc: "12313123", Type: "Savings", Bal: "123.78", Branch: "Bangalore" },
+    { Acc: "12313123", Type: "Deposits", Bal: "123.78", Branch: "Bangalore" },
     { Acc: "12313123", Type: "Savings", Bal: "123.78", Branch: "Bangalore" },
     { Acc: "12313123", Type: "Savings", Bal: "123.78", Branch: "Bangalore" },
+    { Acc: "12313123", Type: "Current", Bal: "123.78", Branch: "Bangalore" },
+    { Acc: "12313123", Type: "OD", Bal: "123.78", Branch: "Bangalore" },
+    { Acc: "12313123", Type: "Current", Bal: "123.78", Branch: "Bangalore" },
+    { Acc: "12313123", Type: "Loans", Bal: "123.78", Branch: "Bangalore" },
+    { Acc: "12313123", Type: "Deposits", Bal: "123.78", Branch: "Bangalore" },
+    { Acc: "12313123", Type: "Savings", Bal: "123.78", Branch: "Bangalore" },
+    { Acc: "12313123", Type: "OD", Bal: "123.78", Branch: "Bangalore" },
     { Acc: "12313123", Type: "Savings", Bal: "123.78", Branch: "Bangalore" },
     { Acc: "12313123", Type: "Savings", Bal: "123.78", Branch: "Bangalore" },
+    { Acc: "12313123", Type: "Loans", Bal: "123.78", Branch: "Bangalore" },
     { Acc: "12313123", Type: "Savings", Bal: "123.78", Branch: "Bangalore" },
-    { Acc: "12313123", Type: "Savings", Bal: "123.78", Branch: "Bangalore" },
-    { Acc: "12313123", Type: "Savings", Bal: "123.78", Branch: "Bangalore" },
-    { Acc: "12313123", Type: "Savings", Bal: "123.78", Branch: "Bangalore" },
-    { Acc: "12313123", Type: "Savings", Bal: "123.78", Branch: "Bangalore" },
-    { Acc: "12313123", Type: "Savings", Bal: "123.78", Branch: "Bangalore" },
-    { Acc: "12313123", Type: "Savings", Bal: "123.78", Branch: "Bangalore" },
-    { Acc: "12313123", Type: "Savings", Bal: "123.78", Branch: "Bangalore" },
-    { Acc: "12313123", Type: "Savings", Bal: "123.78", Branch: "Bangalore" },
-    { Acc: "12313123", Type: "Savings", Bal: "123.78", Branch: "Bangalore" },
-    { Acc: "12313123", Type: "Savings", Bal: "123.78", Branch: "Bangalore" },
-    { Acc: "12313123", Type: "Savings", Bal: "123.78", Branch: "Bangalore" },
-    { Acc: "12313123", Type: "Savings", Bal: "123.78", Branch: "Bangalore" },
-    { Acc: "12313123", Type: "Savings", Bal: "123.78", Branch: "Bangalore" },
-    { Acc: "12313123", Type: "Savings", Bal: "123.78", Branch: "Bangalore" },
-    { Acc: "12313123", Type: "Savings", Bal: "123.78", Branch: "Bangalore" },
-    { Acc: "12313123", Type: "Savings", Bal: "123.78", Branch: "Bangalore" },
-    { Acc: "12313123", Type: "Savings", Bal: "123.78", Branch: "Bangalore" },
+    { Acc: "12313123", Type: "Current", Bal: "123.78", Branch: "Bangalore" },
   ];
+
   const tabs = [
     {
       label: "All Accounts",
-      content: <div><AccountDetails></AccountDetails><Table columns={columns} data={data} loading={false} /></div>,
+      content: (
+        <div>
+          <AccountDetails
+            Balance={AllAccounts.reduce(
+              (acc, curr) => acc + parseFloat(curr.Bal),
+              0
+            ).toFixed(2)}
+          ></AccountDetails>
+          <Table columns={columns} data={AllAccounts} loading={false} />
+        </div>
+      ),
     },
     {
       label: "Saving Account",
-      content: <div><AccountDetails></AccountDetails><Table columns={columns} data={data} loading={false} /></div>,
+      content: (
+        <div>
+          <AccountDetails
+            Balance={AllAccounts.filter((el) => el.Type === "Savings")
+              .reduce((acc, curr) => acc + parseFloat(curr.Bal), 0)
+              .toFixed(2)}
+          ></AccountDetails>
+          <Table
+            columns={columns}
+            data={AllAccounts.filter((el) => el.Type === "Savings")}
+            loading={false}
+          />
+        </div>
+      ),
     },
     {
       label: "Current Account",
-      content: <div><AccountDetails></AccountDetails><Table columns={columns} data={data} loading={false} /></div>,
+      content: (
+        <div>
+          <AccountDetails
+            Balance={AllAccounts.filter((el) => el.Type === "Current")
+              .reduce((acc, curr) => acc + parseFloat(curr.Bal), 0)
+              .toFixed(2)}
+          ></AccountDetails>
+          <Table
+            columns={columns}
+            data={AllAccounts.filter((el) => el.Type === "Current")}
+            loading={false}
+          />
+        </div>
+      ),
     },
     {
       label: "Deposits",
-      content: <div><AccountDetails></AccountDetails><Table columns={columns} data={data} loading={false} /></div>,
+      content: (
+        <div>
+          <AccountDetails
+            Balance={AllAccounts.filter((el) => el.Type === "Deposits")
+              .reduce((acc, curr) => acc + parseFloat(curr.Bal), 0)
+              .toFixed(2)}
+          ></AccountDetails>
+          <Table
+            columns={columns}
+            data={AllAccounts.filter((el) => el.Type === "Deposits")}
+            loading={false}
+          />
+        </div>
+      ),
     },
     {
       label: "Loans",
-      content: <div><AccountDetails></AccountDetails><Table columns={columns} data={data} loading={false} /></div>,
+      content: (
+        <div>
+          <AccountDetails
+            Balance={AllAccounts.filter((el) => el.Type === "Loans")
+              .reduce((acc, curr) => acc + parseFloat(curr.Bal), 0)
+              .toFixed(2)}
+          ></AccountDetails>
+          <Table
+            columns={columns}
+            data={AllAccounts.filter((el) => el.Type === "Loans")}
+            loading={false}
+          />
+        </div>
+      ),
     },
     {
       label: "PPF",
-      content: <div><AccountDetails></AccountDetails><Table columns={columns} data={data} loading={false} /></div>,
+      content: (
+        <div>
+          <AccountDetails
+            Balance={AllAccounts.filter((el) => el.Type === "PPF")
+              .reduce((acc, curr) => acc + parseFloat(curr.Bal), 0)
+              .toFixed(2)}
+          ></AccountDetails>
+          <Table
+            columns={columns}
+            data={AllAccounts.filter((el) => el.Type === "PPF")}
+            loading={false}
+          />
+        </div>
+      ),
     },
     {
       label: "Over Draft",
-      content: <div><AccountDetails></AccountDetails><Table columns={columns} data={data} loading={false} /></div>,
+      content: (
+        <div>
+          <AccountDetails
+            Balance={AllAccounts.filter((el) => el.Type === "OD")
+              .reduce((acc, curr) => acc + parseFloat(curr.Bal), 0)
+              .toFixed(2)}
+          ></AccountDetails>
+          <Table
+            columns={columns}
+            data={AllAccounts.filter((el) => el.Type === "OD")}
+            loading={false}
+          />
+        </div>
+      ),
     },
   ];
-  function AccountDetails() {
+  function AccountDetails({ Balance }) {
     return (
       <div className="bg-white overflow-y-hidden h-full">
-          <div className="flex gap-[30px] pb-4 h-44">
-              <div
-                className="min-w-[100%] bg-gray-200  p-4 rounded-2xl  shadow hover:shadow-lg transition duration-300 ">
-                <div className="flex items-center justify-between mb-2">
-                  <div className="text-sm font-medium text-gray-500">
-                    {"Accounts"}
-                  </div>
-                </div>
-                <div className="text-2xl font-semibold text-gray-900">
-                  {"value"}
-                </div>
-                <div className="text-sm text-gray-400 mt-1">
-                  {"Subtitle"}
-                </div>
+        <div className="flex gap-[30px] pb-4 h-44">
+          <div className="min-w-[100%] bg-gray-200  p-4 rounded-2xl  shadow hover:shadow-lg transition duration-300 ">
+            <div className="flex items-center justify-between mb-2">
+              <div className="text-sm font-medium text-gray-500">
+                {"Accounts"}
               </div>
+            </div>
+            <div className="text-2xl font-semibold text-gray-900">
+              {`Total Balance: ${Balance}`}
+            </div>
+            <div className="text-sm text-gray-400 mt-1">
+              {`Total Balance: ${Balance}`}
+            </div>
+          </div>
         </div>
       </div>
     );
