86d72e28863d9537e5ae88f07c1c2d650174dc33
Sunil.s <sunil.sn@i-exceed.com>
2025-06-05 20:04:41 +0530

    Timer for OTP

--- Frontend/Applications/MicroApplications/auth-microapp/src/Screens/OTPModal.js
+++ Frontend/Applications/MicroApplications/auth-microapp/src/Screens/OTPModal.js
@@ -1,15 +1,55 @@
 import React, { useEffect, useState, useRef } from "react";
 
-function OTPModal({Request, onSuccess, onCancel, onFailure, onRegenOTP, SecurityParams}) {
-  const OTPLength = SecurityParams[0].OTPLength;
-  const [otp, setOtp] = useState(Array(OTPLength).fill(""));
+function OTPModal({
+  Request,
+  onSuccess,
+  onCancel,
+  onFailure,
+  onRegenOTP,
+  SecurityParams,
+}) {
+  const OTPData = SecurityParams[0];
+  const [otp, setOtp] = useState(Array(OTPData.OTPLength).fill(""));
+  const [resendOTP, setResendOTP] = useState(false);
+  const [regenCount, setRegenCount] = useState(0);
+  const [seconds, setSeconds] = useState(OTPData.OTPTimer);
   const inputsRef = useRef([]);
-
   useEffect(() => {
     setTimeout(() => {
       inputsRef.current[0]?.focus();
     }, 100);
   }, []);
+useEffect(() => {
+  if (seconds <= 0) {
+    setRegenCount((prev) => {
+      const next = prev + 1;
+      if (next >= OTPData.OTPRegencount) {
+        window.showAlert(false);
+        window.showAlert({
+          AlertType: "E",
+          AlertDesc:
+            "You have Exhausted all the attempts Left, Please re-login",
+          Btns: [
+            {
+              Name: "Ok",
+              function: () =>
+                window.launchMicroApp("login", "LoginPage", "BaseScreenID"),
+            },
+          ],
+        });
+      } else {
+        setResendOTP(true);
+      }
+      return next;
+    });
+    return;
+  }
+  const interval = setInterval(() => {
+    setSeconds((prev) => prev - 1);
+  }, 1000);
+
+  return () => clearInterval(interval);
+}, [seconds, OTPData.OTPRegencount]);
 
   const handleChange = (e, index) => {
     const value = e.target.value.replace(/\D/g, "");
@@ -19,32 +59,44 @@ function OTPModal({Request, onSuccess, onCancel, onFailure, onRegenOTP, Security
     newOtp[index] = value;
     setOtp(newOtp);
 
-    if (value && index < OTPLength - 1) {
+    if (value && index < OTPData.OTPLength - 1) {
       setTimeout(() => {
         inputsRef.current[index + 1]?.focus();
-        
       }, 0);
     }
-    if(newOtp[OTPLength - 1] !== "" && index == OTPLength - 1){
-        handleSubmit(newOtp);
+    if (
+      newOtp[OTPData.OTPLength - 1] !== "" &&
+      index == OTPData.OTPLength - 1
+    ) {
+      handleSubmit(newOtp);
     }
   };
 
   const handleSubmit = (newOtp) => {
     console.log(newOtp);
-  }
+  };
 
   const handleKeyDown = (e, index) => {
     if (e.key === "Backspace" && !otp[index] && index > 0) {
       setTimeout(() => {
         inputsRef.current[index - 1]?.focus();
-        if(inputsRef.current[index].value == ""){
-            inputsRef.current[index - 1].value = "";
-            otp[index - 1] = "";
+        if (inputsRef.current[index].value == "") {
+          inputsRef.current[index - 1].value = "";
+          otp[index - 1] = "";
         }
       }, 0);
-    } 
+    }
+  };
+  const formatTime = (sec) => {
+    const m = String(Math.floor(sec / 60)).padStart(2, "0");
+    const s = String(sec % 60).padStart(2, "0");
+    return `${m}:${s}`;
+  };
 
+  const ResendOTP = () => {
+     if (regenCount >= OTPData.OTPRegencount) return;
+    setSeconds(OTPData.OTPTimer);
+    setResendOTP(false);
   };
 
   return (
@@ -58,13 +110,25 @@ function OTPModal({Request, onSuccess, onCancel, onFailure, onRegenOTP, Security
             type="text"
             inputMode="numeric"
             maxLength="1"
-            className="w-10 h-12 text-center border rounded-2xl text-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            className="w-10 h-12 text-center border rounded-2xl text-lg focus:outline-none focus:ring-2 border border-gray-200 shadow-sm bg-white focus:ring-blue-500"
             value={digit}
             onChange={(e) => handleChange(e, index)}
             onKeyDown={(e) => handleKeyDown(e, index)}
           />
         ))}
       </div>
+      {resendOTP ? (
+        <div
+          className="text-center text-blue-600 font-medium cursor-pointer hover:text-gray-500"
+          onClick={() => ResendOTP()}
+        >
+          Resend OTP
+        </div>
+      ) : (
+        <div className=" animate-breathe text-center text-gray-500 font-medium hover:text-blue-600">
+          Time remaining: {formatTime(seconds)}
+        </div>
+      )}
     </div>
   );
 }
--- Frontend/Applications/base-application/tailwind.config.js
+++ Frontend/Applications/base-application/tailwind.config.js
@@ -20,10 +20,15 @@ module.exports = {
           "0%": { opacity: "0", transform: "translateX(-30px)" },
           "100%": { opacity: "1", transform: "translateX(0)" },
         },
+        "breathe": {
+          '0%, 100%': { transform: 'scale(1.25)', opacity: '1' },
+          '50%': { transform: 'scale(1.05)', opacity: '0.85' },
+        },
       },
       animation: {
         "slide-in-left": "slide-in-left 0.5s ease-out forwards",
         "slide-in-right": "slide-in-right 0.5s ease-out forwards",
+         "breathe": 'breathe 2s ease-in-out infinite',
       },
     },
   },
